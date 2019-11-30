2e0c057e051933872ebbd140deef7a7c4a632f3d
Denis Glotov <denis@glotov.org>
2019-11-30 22:34:51 +0200

    Development:
    * pip fix, install-src.sh
    * readme upd
    * optionally use 'dev' branch of .emacs.d

---
+++ install-src.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip3 install --user setuptools
+pip3 install --user dymka
+
+mkdir src
--- install-user.sh
+++ install-user.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -xe
 
 cd
-git clone --branch dev https://github.com/denisglotov/.emacs.d
+git clone https://github.com/denisglotov/.emacs.d
 cd .emacs.d
+git checkout dev || true
 bin/install_emacs.sh 26
 bin/install_tmux.sh -s
 
@@ -13,7 +14,11 @@ mv /tmp/id_rsa.pub .ssh/id_rsa.pub
 chmod 600 .ssh/id_rsa.pub
 
 git config --global user.name "Denis Glotov"
+git config --global user.email denis@glotov.org
 
 pip3 install --user -U pip
-pip3 install --user setuptools
-pip3 install --user dymka
+
+cp /tmp/boo/install-src.sh .
+
+echo "[INFO] All done. Please log out and log in so local changes take effect."
+echo "       Then use ./install-src.sh"
